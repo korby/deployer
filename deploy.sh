28ea24cccad464f66ae26d9f4885124c47e7d490
a.cianfarani <a.cianfarani@c2is.fr>
2016-03-03 19:43:50 +0100

    Wip...

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 abs_path=$(dirname "$0")
+log_dir=./logs
+copy_dir=./tmp
+if [ ! -d "$log_dir" ] ; then mkdir -p $log_dir; fi
 
 . $abs_path/lib/yaml_parse
 . $abs_path/lib/colors
-. $abs_path/lib/exec_remote
+. $abs_path/lib/exec_wrap
+. $abs_path/lib/trap
+
+trap "on_exit" SIGINT SIGTERM INT TERM EXIT;
 #parse_yaml vhosts.yml
 #parse_yaml hosts.yml
 
@@ -13,6 +19,8 @@ ids=()
 eval $(parse_yaml hosts.yml)
 hosts=(${ids[@]})
 
+release_name=$(date +%Y%m%d%H%M%S)
+log_file=$log_dir"/"$release_name".log"
 
 action=""
 if [ $# -eq 0 ] ; then
@@ -20,12 +28,25 @@ if [ $# -eq 0 ] ; then
 	echo -e "and deployed according this settings :"
     action=each_info
 fi
-case $1 in
+
+switcher=$1
+while getopts d options
+do	case "$options" in
+    d)  debug=1
+		switcher=$2
+		;;
+	esac
+done
+case $switcher in
     test )
         action=each_deploy_test ;;
+    deploy )
+	echo -e "$green Deploying release $release_name$std";
+        action=each_deploy ;;
 esac
 
-now=$(date +%Y%m%d%H%M%S)
+
+
 counter=1
 for id in "${vhosts[@]}"
 do
@@ -35,7 +56,7 @@ do
 	tasks="vhosts_"$id"_tasks"
 	eval "tasks=(\${$tasks[@]})"
 
-	release_path=${!deploy_to}releases/$now/
+	release_path=${!deploy_to}releases/$release_name/
 	shared_path=${!deploy_to}shared/
 
 	. $abs_path/lib/$action
