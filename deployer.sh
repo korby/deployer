9eaf06919b5013965df63b35ea6afde10c14f254
a.cianfarani <a.cianfarani@c2is.fr>
2016-03-25 10:45:32 +0100

    Now use shift to handle options and some indent changes because of new text editor

--- deployer.sh
+++ deployer.sh
@@ -26,56 +26,60 @@ release_name=$(date +%Y%m%d%H%M%S)
 log_file=$log_dir"/"$release_name".log"
 
 action=""
-
-switcher=$1
+option_total=0
 while getopts ds options
 do	case "$options" in
     d)  debug=1
-		switcher=$2
+        (( option_total++ ))
 		;;
-	s)  simulate=1
-		switcher=$2
-		;;
+  	s)  simulate=1
+        (( option_total++ ))
+  		;;
 	esac
 done
-case $switcher in
-	init )
-		read -p "Example config files will be written here $(echo -e $yellow `pwd` $std) [N,y] ? " agree
-			case $agree in
-				"y" | "Y" | "yes" | "Yes")
-					. $abs_path/lib/init;;
-				*)
-					track "screen" "Aborted."; ;;
-			esac
-         exit 1;;
+for (( c=1; c<=$option_total; c++ ))
+do
+   shift;
+done
+
+case $1 in
+	 init )
+  		read -p "Example config files will be written here $(echo -e $yellow `pwd` $std) [N,y] ? " agree
+  			case $agree in
+  				"y" | "Y" | "yes" | "Yes")
+  					. $abs_path/lib/init;;
+  				*)
+  					track "screen" "Aborted."; ;;
+  			esac
+           exit 1;;
 
     test )
         action=each_deploy_test ;;
 
     deploy )
-	track "screen" $green"Deploying release $release_name and loging in $log_file"$std;
-    action=each_deploy ;;
+	     track "screen" $green"Deploying release $release_name and loging in $log_file"$std;
+       action=each_deploy ;;
 
     rollback )
-	track "screen" $green"Rollbacking $release_name and loging in $log_file"$std;
-    action=each_rollback ;;
+	     track "screen" $green"Rollbacking $release_name and loging in $log_file"$std;
+       action=each_rollback ;;
 
     exec )
-		if [ "$debug" == 1 ]; then track "warning" "-d not allowed with $switcher"; exit 1; fi;
+    		if [ "$debug" == 1 ]; then track "warning" "-d not allowed with $1"; exit 1; fi;
 
-		track "warning" "That Command will be executed on each server for each vhost (don't forget you can use these kind of replacement: %deploy_to, %shared_path etc.)!";
-		read -p "Sure to execute it [N,y] ?" agree
-		case $agree in
-			"y" | "Y" | "yes" | "Yes")
-				to_exec=${@:2}
-	    		action=each_exec;;
-			*) track "info" "Aborted."; exit 1;;
-		esac
-	 ;;
+    		track "warning" "That Command will be executed on each server for each vhost (don't forget you can use these kind of replacement: %deploy_to, %shared_path etc.)!";
+    		read -p "Sure to execute it [N,y] ?" agree
+    		case $agree in
+    			"y" | "Y" | "yes" | "Yes")
+    				to_exec=${@:2}
+    	    		action=each_exec;;
+    			*) track "info" "Aborted."; exit 1;;
+    		esac
+    	 ;;
 
   	"")
-		# If not args
-    	action=each_info
+  		  # If not args
+      	action=each_info
         ;;
     *)
         track "screen" "Unknown action";
